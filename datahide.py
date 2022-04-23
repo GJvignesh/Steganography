ce37a70e8e2ff99075cf4eecba3dfc4c0348c41d
GJvignesh <47006122+GJvignesh@users.noreply.github.com>
2022-04-23 06:47:28 +0530

    Working version
    this is a working demo after fixing the char map Unicode error using the IO wrapper filename.name

--- datahide.py
+++ datahide.py
@@ -3,85 +3,89 @@ from tkinter import filedialog
 import tkinter as tk
 from PIL import Image, ImageTk
 import os
-from stegano import lsb #pip install stegano
+from stegano import lsb  # pip install stegano
 
-root=Tk()
+root = Tk()
 root.title("Steganography - Hide a Secret Text Message in an Image")
-root.geometry("700*500+250+180")
-root.resizable(False,False)
-root.configure(bg="2f4155")
+root.geometry("700x500+250+180")
+root.resizable(False, False)
+root.configure(bg="#2f4155")
+
 
 def showimage():
     global filename
-    filename=filedialog.askopenfile(initialdir=os.getcwd(),
+    filename = filedialog.askopenfile(initialdir=os.getcwd(),
                                       title='Select Image file',
-                                      filetype=(("PNG file","*.png"),
-                                      ("JPG file","*.jpg"),("All file","*.txt")))
-    img=Image.open(filename)
-    img=ImageTk.photoImage(img)
-    lbl.configure(image=img,width=250,height=250)
-    lbl.image=img
+                                      filetype=(("PNG file", " *.png"),
+                                                ("JPG file", "*.jpg"), ("All file", "*.txt")))
+    #print(filename)
+    file_in_bytestream = open(filename.name, 'rb')
+    img = Image.open(file_in_bytestream)
+    img = ImageTk.PhotoImage(img)
+    lbl.configure(image=img, width=250, height=250)
+    lbl.image = img
 
-def Hide():
+
+def hide():
     global secret
-    message=text1.get(1.0,END)
-    secret = lsb.hide(str(filename),message)
+    message = text1.get(1.0, END)
+    secret = lsb.hide(str(filename.name), message)
 
-def Show():
-    clear_message = lsb.reveal(filename)
-    text1.delete(1.0,END)
+
+def show():
+    clear_message = lsb.reveal(filename.name)
+    text1.delete(1.0, END)
     text1.insert(END, clear_message)
-  
+
+
 def save():
     secret.save("hidden.png")
 
-#icon 
-image_icon=PhotoImage(file="logo.jpg")
-root.iconphoto(False,image_icon)
 
-#logo
-logo=PhotoImage(file="logo.png")
-Label(root,image=logo,bg="#2f4155").place(x=10,y=0)
+# icon
+image_icon = ImageTk.PhotoImage(file=r"C:\\Users\\vignesh.jayalakshmi\\Downloads\\logo.jpg")
+root.iconphoto(False, image_icon)
 
-Label(root,text="CYBER SCIENCE",bg="#2d4155",fg="white",font="arial 25 bold").place(x=100,y=20)
+# logo
+logo = PhotoImage(file=r"C:\\Users\\vignesh.jayalakshmi\\Downloads\\logo.png")
+Label(root, image=logo, bg="#2f4155").place(x=10, y=0)
 
-#first frame
-f=Frame(root,bd=3,bg="black",width=340,height=280,relief=GROOVE)
-f.place(x=10,y=80)
+Label(root, text="CYBER SCIENCE", bg="#2d4155", fg="white", font="arial 25 bold").place(x=100, y=20)
 
-lbl=Label(f,bg="black")
-lbl.place(x=40,y=10)
+# first frame
+f = Frame(root, bd=3, bg="black", width=340, height=280, relief=GROOVE)
+f.place(x=10, y=80)
 
-#Second Frame
-frame2=Frame(root,bd=3,width=340,height=280,bg="white",relief=GROOVE)
-frame2.place(x=350,y=80)
+lbl = Label(f, bg="black")
+lbl.place(x=40, y=10)
 
-text1=Text(frame2,font="Robote 20",bg="white",fg="black",relief=GROOVE,wrap=WORD)
-text1.place(x=0,y=0,width=320,height=295)
+# Second Frame
+frame2 = Frame(root, bd=3, width=340, height=280, bg="white", relief=GROOVE)
+frame2.place(x=350, y=80)
 
-scrollbar1=Scrollbar(frame2)
-scrollbar1.place(x=320,y=0,height=300)
+text1 = Text(frame2, font="Robote 20", bg="white", fg="black", relief=GROOVE, wrap=WORD)
+text1.place(x=0, y=0, width=320, height=295)
+
+scrollbar1 = Scrollbar(frame2)
+scrollbar1.place(x=320, y=0, height=300)
 
 scrollbar1.configure(command=text1.yview)
 text1.configure(yscrollcommand=scrollbar1.set)
 
-#third Frame
-frame3=Frame(root,bd=3,bg="#2f4155",width=330,height=100,relief=GROOVE)
-frame3.place(x=10,y=370)
+# third Frame
+frame3 = Frame(root, bd=3, bg="#2f4155", width=330, height=100, relief=GROOVE)
+frame3.place(x=10, y=370)
 
-Button(frame3,text="Open Image",width=10,height=2,font="arial 14 bold",command=showimage).place(x=20,y=30)
-Button(frame3,text="Save Image",width=10,height=2,font="arial 14 bold",command=save).place(x=180,y=30)
-Label(frame3,text="Picture,Image,Photo File",bg="#2f4155",fg="yellow").place(x=20,y=5)
+Button(frame3, text="Open Image", width=10, height=2, font="arial 14 bold", command=showimage).place(x=20, y=30)
+Button(frame3, text="Save Image", width=10, height=2, font="arial 14 bold", command=save).place(x=180, y=30)
+Label(frame3, text="Picture,Image,Photo File", bg="#2f4155", fg="yellow").place(x=20, y=5)
 
-#fourth Frame
-frame4=Frame(root,bd=3,bg="#2f4155",width=330,height=100,relief=GROOVE)
-frame4.place(x=360,y=370)
+# fourth Frame
+frame4 = Frame(root, bd=3, bg="#2f4155", width=330, height=100, relief=GROOVE)
+frame4.place(x=360, y=370)
 
-Button(frame4,text="Hide Data",width=10,height=2,font="arial 14 bold",command=Hide).place(x=20,y=30)
-Button(frame4,text="Show Data",width=10,height=2,font="arial 14 bold",command=show).place(x=180,y=30)
-Label(frame4,text="Picture,Image,Photo File",bg="#2f4155",fg="yellow").place(x=20,y=5)
-
-
-
+Button(frame4, text="Hide Data", width=10, height=2, font="arial 14 bold", command=hide).place(x=20, y=30)
+Button(frame4, text="Show Data", width=10, height=2, font="arial 14 bold", command=show).place(x=180, y=30)
+Label(frame4, text="Picture,Image,Photo File", bg="#2f4155", fg="yellow").place(x=20, y=5)
 
 root.mainloop()
